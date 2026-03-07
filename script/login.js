224fa3ee48aa3def86c7342468762bc96dceb201
tushariqbal1 <tushariqbal331@gmail.com>
2026-03-07 08:47:11 +0600

    make login form functionality

--- script/login.js
+++ script/login.js
@@ -1,1 +1,17 @@
-console.log()
+document.getElementById('login-btn').addEventListener('click', function () {
+    const inputUser = document.getElementById('input-username');
+    const userName = inputUser.value;
+    
+    const inputPassword = document.getElementById('input-password');
+    const userPassword = inputPassword.value;
+
+    if(userName == 'admin' && userPassword == 'admin123'){
+        alert('Login successful')
+
+        window.location.assign('/home.html')
+    }
+    else{
+        alert('Login failed');
+        return
+    }
+})
